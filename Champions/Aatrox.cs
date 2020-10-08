d03c217d5f3441cf8ffb2f22c10331b6326c435e
Andrew <antongyang@yahoo.com>
2020-10-08 13:04:07 -0600

    Created Aatrox champion class.

---
+++ Champions/Aatrox.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Champions
+{
+    class Aatrox : Champion
+    {
+        /// <summary><para>Deathbringer Stance</para>
+        /// <para>Next basic attack deals 5%-12% of the targer's maximum health based on their level as bonus physical damage.</para>
+        /// <para>Heals Aatrox for 100% of the bonus damage dealt.</para></summary>
+        /// 
+        /// <param name="enemy_Level">The level of the enemy champion.</param>
+        /// <param name="enemy_maxHP">The maximum HP of the enemy champion.</param>
+        /// 
+        /// <returns>Returns a string containting the amount of damage dealt.</returns>
+        ///
+        public double Passive(string enemy_Level, double enemy_maxHP)
+        {
+            int eLevel = 0;
+            int.TryParse(enemy_Level, out eLevel);
+            double percent_health = (4.588 + (0.412 * eLevel)) / 100;
+            double damage_dealt = enemy_maxHP * percent_health;
+            return damage_dealt;
+        }
+
+        /// <summary><para>The Darkin Blade</para>
+        /// <para>Sweetspot = 50% bonus damage</para>
+        /// <para>Q2 = 1.25*Q1, Q3 = 1.5*Q1</para>
+        /// <para>Q1: 10/30/50/70/90 + 60/65/70/75/80 % AD</para>
+        /// <para>Q2: 12.5/37.5/62.5/87.5/112.5 + 75/81.25/93.75/100 % AD</para>
+        /// <para>Q3: 15/45/75/105/135 + 90/97.5/105/112.5/120 % AD</para></summary>
+        ///
+        /// <param name="rank">The rank of the ability.</param>
+        /// 
+        /// <returns>Returns a list of doubles in the order of Q1-normal, Q1-sweetspot, Q2-normal, Q2-sweetspot, Q3-normal, Q3-sweetspot.</returns>
+        ///
+        public List<double> Q(int rank)
+        {
+            double Q1_base = 10 + ((rank - 1) * 20);
+            double Q1_scaling = (60 + ((rank - 1) * 5)) / 100;
+
+            double Q2_base = Q1_base * 1.25;
+            double Q2_scaling = Q1_scaling * 1.25;
+
+            double Q3_base = Q1_base * 1.5;
+            double Q3_scaling = Q1_scaling * 1.5;
+
+            double Q1_damage_normal = Q1_base + Q1_scaling * CalculateAD(Level);
+            double Q3_damage_normal = Q2_base + Q2_scaling * CalculateAD(Level);
+            double Q2_damage_normal = Q3_base + Q3_scaling * CalculateAD(Level);
+
+            double Q1_damage_sweet = Q1_damage_normal * 1.5;
+            double Q2_damage_sweet = Q2_damage_normal * 1.5;
+            double Q3_damage_sweet = Q3_damage_normal * 1.5;
+
+            return new List<double>()
+            { Q1_damage_normal, Q1_damage_sweet, Q2_damage_normal, Q2_damage_sweet, Q3_damage_normal, Q3_damage_sweet };
+        }
+
+        /// <summary>
+        /// <para>Infernal Chains</para>
+        /// <para>W: 30/40/50/60/70 + 40% AD</para>
+        /// </summary>
+        /// <param name="rank">The rank of the ability as an int.</param>
+        /// <returns>The damage dealt as a double.</returns>
+        public double W(int rank)
+        {
+            double W_base = 30 + ((rank - 1) * 10);
+            double W_scaling = .4;
+            double W_damage = W_base + W_scaling * CalculateAD(Level);
+            return W_damage;
+        }
+        
+        /// <summary>
+        /// World Ender
+        /// <para>Gains 20/30/40 % AD</para>
+        /// </summary>
+        /// <param name="rank">The rank of the ability as an int.</param>
+        /// <returns>The amount of bonus AD Aatrox gains as a double.</returns>
+        public double R(int rank)
+        {
+            double R_bAD_scaling = .2 + ((rank - 1) * .1);
+            double R_bAD = CalculateAD(Level);
+            return R_bAD;
+        }
+
+    }
+}
---
+++ Champions/Champion.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Champions
+{
+    public class Champion
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public string Health { get; set; }
+        public string HealthRegen { get; set; }
+        public string Mana { get; set; }
+        public string ManaRegen { get; set; }
+        public string Manaless { get; set; }
+        public string AutoRange { get; set; }
+        public string MovementSpeed { get; set; }
+        public string AttackDamage { get; set; }
+        public string AttackSpeed { get; set; }
+        public string Armor { get; set; }
+        public string MagicResist { get; set; }
+        public string Health_Growth { get; set; }
+        public string HealthRegen_Growth { get; set; }
+        public string Mana_Growth { get; set; }
+        public string ManaRegen_Growth { get; set; }
+        public string AD_Growth { get; set; }
+        public string AS_Growth { get; set; }
+        public string Armor_Growth { get; set; }
+        public string MR_Growth { get; set; }
+        public string AbilityHaste { get; set; }
+
+        /// <summary>
+        /// Note: does not take into account Armor and MR when outputing damage numbers.
+        /// </summary>
+        public Champion()
+        {
+        }
+        public Champion(string name)
+        {
+            this.Name = name;
+        }
+
+        public double CalculateHealth(int n)
+        {
+            return Double.Parse(Health) + Double.Parse(Health_Growth) * (n - 1) * (0.7025 + 0.0175 * (n - 1));
+        }
+        public double CalculateHealthRegen(int n)
+        {
+            return Double.Parse(HealthRegen) + Double.Parse(HealthRegen_Growth) * (n - 1) * (0.7025 + 0.0175 * (n - 1));
+        }
+        public double CalculateMana(int n)
+        {
+            return Double.Parse(Mana) + Double.Parse(Mana_Growth) * (n - 1) * (0.7025 + 0.0175 * (n - 1));
+        }
+        public double CalculateManaRegen(int n)
+        {
+            return Double.Parse(ManaRegen) + Double.Parse(ManaRegen_Growth) * (n - 1) * (0.7025 + 0.0175 * (n - 1));
+        }
+        public double CalculateAD(int n) //TODO: add items to these functions
+        {
+            return Double.Parse(AttackDamage) + Double.Parse(AD_Growth) * (n - 1) * (0.7025 + 0.0175 * (n - 1));
+        }
+        public double CalculateBonusAS(int n)
+        {
+            if (Name == "Gnar Mini")
+                return Double.Parse(AS_Growth) * (n - 1) * (0.7025 + 0.0175 * (n - 1) + 0.055);
+            else
+                return Double.Parse(AS_Growth) * (n - 1) * (0.7025 + 0.0175 * (n - 1));
+        }
+        public double CalculateArmor(int n)
+        {
+            return Double.Parse(Armor) + Double.Parse(Armor_Growth) * (n - 1) * (0.7025 + 0.0175 * (n - 1));
+        }
+        public double CalculateMR(int n)
+        {
+            return Double.Parse(MagicResist) + Double.Parse(MR_Growth) * (n - 1) * (0.7025 + 0.0175 * (n - 1));
+        }
+        public string CalculateRange(int n)
+        {
+            if (Name == "Tristana")
+                return (8 * (n - 1) + Double.Parse(AutoRange)).ToString();
+            else if (Name == "Gnar Mini")
+                return (Double.Parse(AutoRange) + 50 + 5.9 * (n - 1)).ToString();
+            else
+                return AutoRange;
+        }
+
+            
+    }
+}
