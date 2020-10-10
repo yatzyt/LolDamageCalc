721561af0260a82d01f3e0d9fed8355e8863011d
Andrew <antongyang@yahoo.com>
2020-10-10 01:04:44 -0600

    Set up structure for items in S11

---
+++ Items/ItemTier.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Items
+{
+    class ItemTier
+    {
+        public static ItemTier Mythic { get; internal set; }
+    }
+}
---
+++ Items/Mythic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Items
+{
+    class Mythic : Item
+    {
+        public ItemTier tier = ItemTier.Mythic;
+
+        public Mythic()
+        {
+            tier = new ItemTier();
+        }
+    }
+}
