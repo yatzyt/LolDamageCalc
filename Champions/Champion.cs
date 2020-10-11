fe1b8729a9f141574e6c21f5095354db597b2dd4
Andrew <antongyang@yahoo.com>
2020-10-11 14:06:58 -0600

    Fleshed out Aatrox profile

--- Champions/Champion.cs
+++ Champions/Champion.cs
@@ -2,66 +2,67 @@
 using System.Collections.Generic;
 using System.Text;
 using Items;
+using System.Linq;
 
 namespace Champions
 {
     public class Champion
     {
-        public string Name { get; }
-        public int Level { get; }
+        public string Name { get; internal set; }
+        public int Level { get; set; }
 
         // Health block
-        public double HP { get; }
-        public double HPRegen { get; }
-        public double Health_Growth { get; }
-        public double HealthRegen_Growth { get; }
+        public double HP { get; internal set; }
+        public double HPRegen { get; internal set; }
+        public double Health_Growth { get; internal set; }
+        public double HealthRegen_Growth { get; internal set; }
 
         // Resource block. Mana = energy
-        public double Mana { get; }
-        public double ManaRegen { get; }
-        public double Mana_Growth { get; }
-        public double ManaRegen_Growth { get; }
-        public bool Manaless { get; }
+        public double Mana { get; internal set; }
+        public double ManaRegen { get; internal set; }
+        public double Mana_Growth { get; internal set; }
+        public double ManaRegen_Growth { get; internal set; }
+        public bool Manaless { get; internal set; }
 
         // Auto block
-        public double AutoRange { get; }
+        public double AutoRange { get; internal set; }
 
         // Movement block
-        public double MovementSpeed { get; }
-        public double MovementSpeedPercent { get; }
+        public double MovementSpeed { get; internal set; }
+        public double MovementSpeedPercent { get; internal set; }
 
         // AD block
-        public double AD { get; }
-        public double AD_Growth { get; }
-        public double AttackSpeed { get; }
-        public double AttackSpeed_Growth { get; }
-        public double CritDamage { get; }
-        public double CritChance { get; }
-        public double Lethality { get; }
-        public double ArmorPen { get; }
-        public double BonusArmorPen { get; }
+        public double AD { get; internal set; }
+        public double AD_Growth { get; internal set; }
+        public double AttackSpeed { get; internal set; }
+        public double AttackSpeed_Growth { get; internal set; }
+        public double CritDamage { get; internal set; } // This is a multiplier
+        public double CritChance { get; internal set; }
+        public double Lethality { get; internal set; }
+        public double ArmorPen { get; internal set; }
+        public double BonusArmorPen { get; internal set; }
 
         // AP block
-        public double AP { get; }
-        public double AbilityHaste { get; }
-        public double MagicPenFlat { get; }
-        public double MagicPenPerc { get; }
-        public double BonusMagicPenPerc { get; }
+        public double AP { get; internal set; }
+        public double AbilityHaste { get; internal set; }
+        public double MagicPenFlat { get; internal set; }
+        public double MagicPenPerc { get; internal set; }
+        public double BonusMagicPenPerc { get; internal set; }
 
         // Resistances block
-        public double Armor { get; }
-        public double MR { get; }
-        public double Armor_Growth { get; }
-        public double MR_Growth { get; }
+        public double Armor { get; internal set; }
+        public double MR { get; internal set; }
+        public double Armor_Growth { get; internal set; }
+        public double MR_Growth { get; internal set; }
 
         // Misc block
-        public double Tenacity { get; }
-        public double PhysicalVamp { get; }
-        public double Omnivamp { get; }
-        public double HealAndShieldPower { get; }
+        public double Tenacity { get; internal set; }
+        public double PhysicalVamp { get; internal set; }
+        public double Omnivamp { get; internal set; }
+        public double HealAndShieldPower { get; internal set; }
 
         // Items
-        public List<Item> Inventory { get; set; }
+        public Dictionary<int, Item> Inventory { get; set; }
 
         /// <summary>
         /// Note: does not take into account Armor and MR when outputing damage numbers.
@@ -78,54 +79,67 @@ namespace Champions
         {
             return HP + Health_Growth * (Level - 1) * (0.7025 + 0.0175 * (Level - 1));
         }
+
         public double CalculateHealthRegen(int n)
         {
             return HPRegen + HealthRegen_Growth * (n - 1) * (0.7025 + 0.0175 * (n - 1));
         }
+
         public double CalculateMana(int n)
         {
             return Mana + Mana_Growth * (n - 1) * (0.7025 + 0.0175 * (n - 1));
         }
+
         public double CalculateManaRegen(int n)
         {
             return ManaRegen + ManaRegen_Growth * (n - 1) * (0.7025 + 0.0175 * (n - 1));
         }
+
         public double CalculateAD(int n) //TODO: add items to these functions
         {
             return AD + AD_Growth * (n - 1) * (0.7025 + 0.0175 * (n - 1));
         }
+
         public double CalculateBonusAD(int n)
         {
             return 0;
         }
+
         public double CalculateLethality(int n)
         {
             return 0;
         }
+
         public double CalculatePercentArmorPen(int n)
         {
             return 0;
         }
+
         public double CalculatePercentBonusArmorPen(int n)
         {
             return 0;
         }
+
         public double CalculateAP(int n)
         {
             return 0;
         }
+
         public double CalculateFlatMagicPen(int n)
         {
             return 0;
         }
+
         public double CalculatePercentMagicPen(int n)
         {
             return 0;
         }
+
         public double CalculatePercentBonusMagicPen(int n)
         {
             return 0;
         }
+
         public double CalculateBonusAS(int n)
         {
             if (Name == "Gnar Mini")
@@ -133,14 +147,17 @@ namespace Champions
             else
                 return AttackSpeed_Growth * (n - 1) * (0.7025 + 0.0175 * (n - 1));
         }
+
         public double CalculateArmor(int n)
         {
             return Armor + Armor_Growth * (n - 1) * (0.7025 + 0.0175 * (n - 1));
         }
+
         public double CalculateMR(int n)
         {
             return MR + MR_Growth * (n - 1) * (0.7025 + 0.0175 * (n - 1));
         }
+
         public double CalculateRange(int n)
         {
             if (Name == "Tristana")
@@ -151,10 +168,20 @@ namespace Champions
                 return AutoRange;
         }
 
-        public object GetChampion(string championName)
+        public double ConvertHasteToCDR()
+        {
+            return 1 - (1 / (1 + (AbilityHaste / 100)));
+        }
+
+        public static object GetChampion(string championName)
         {
             Type t = Type.GetType(championName);
-            return Activator.CreateInstance(t);
+            if (championName.Contains("."))
+            {
+                championName = championName.Split('.').Last();
+            }
+            object[] args = new object[] { championName, 1 };
+            return Activator.CreateInstance(t, args);
         }
             
     }
